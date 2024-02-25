16b5ba35ae6d2f1e0638d69c2de2c17495217bb8
Gaël Stervinou <stervinou.g36@gmail.com>
2024-02-25 15:45:56 +0100

    Project

--- squizz-front/src/app/page.js
+++ squizz-front/src/app/page.js
@@ -1,7 +1,34 @@
-import Image from 'next/image'
+'use client';
+
+import Link from 'next/link';
+import { jwtDecode } from 'jwt-decode';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div>TRIPLE MONSTRE</div>
-  )
+  const [userToken, setUserToken] = useState(null);
+  const [decodedToken, setDecodedToken] = useState(null);
+  useEffect(() => {
+    setUserToken(localStorage.getItem('token'));
+  }, []);
+  useEffect(() => {
+    if (userToken) {
+      setDecodedToken(jwtDecode(userToken));
+    }
+  }, [userToken]);
+  return <>
+    {!userToken && <Link href="/login">Se connecter</Link>}
+    {decodedToken && decodedToken.role === 'teacher' &&
+      <div>
+        <Link href="/quizz/create">Créer un quizz</Link>
+        <Link href="/quizz/list">Voir mes quizz</Link>
+        <Link href="/logout">Se déconnecter</Link>
+      </div>
+    }
+    {decodedToken && decodedToken.role === 'student' &&
+      <div>
+        <Link href="/quizz/play">Jouer</Link>
+        <Link href="/logout">Se déconnecter</Link>
+      </div>
+    }
+  </>;
 }
